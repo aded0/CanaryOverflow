65ed5eb19530ca20ba5ace7086ab015f592901d2
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2022-04-28 22:28:14 +0500

    start adding profile aggregate

---
+++ src/CanaryOverflow.Domain/ProfileAggregate/Profile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using CanaryOverflow.Common;
+
+namespace CanaryOverflow.Domain.ProfileAggregate;
+
+public class Profile : AggregateRoot<Guid, Profile>
+{
+    public Profile()
+    {
+    }
+
+    public string DisplayName { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid AvatarId { get; set; }
+    public string Summary { get; set; }
+    public string Email { get; set; }
+    public HashSet<Guid> Answers { get; set; }
+    public HashSet<Guid> Questions { get; set; }
+
+    protected override void When(IDomainEvent @event)
+    {
+    }
+}
