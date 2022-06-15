a7aa45c6cee0d02810d23e1e7b4f6102a461599a
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2022-06-15 17:22:47 +0500

    redo db context, upd. deps

---
+++ src/CanaryOverflow.Infrastructure/Data/CanaryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CanaryOverflow.Infrastructure.Data;
+
+public class CanaryDbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+{
+    public CanaryDbContext(DbContextOptions<CanaryDbContext> options) : base(options)
+    {
+    }
+}
