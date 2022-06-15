5c4ebfdcd9f358cf2e666c30520ddc8f82ed6c54
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2022-06-16 02:51:32 +0500

    add gulp, del. prev. frontend configs

---
+++ src/CanaryOverflow.Service.MVC/gulpfile.js
@@ -0,0 +1,15 @@
+﻿const {series, src, dest} = require("gulp");
+const del = require("del");
+const concat = require("gulp-concat");
+
+function cleanWwwroot(done) {
+  return del("wwwroot/*", done);
+}
+
+function cssBundle() {
+  return src("Views/**/*.css")
+    .pipe(concat("styles.css"))
+    .pipe(dest("wwwroot"));
+}
+
+exports.default = series(cleanWwwroot, cssBundle)
