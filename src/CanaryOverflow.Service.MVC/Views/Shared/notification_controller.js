12b4953dbed3d6a7e65744876d310b2712390da5
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2022-06-16 17:56:08 +0500

    add hiding notification

--- src/CanaryOverflow.Service.MVC/Views/Shared/notification_controller.js
+++ src/CanaryOverflow.Service.MVC/Views/Shared/notification_controller.js
@@ -1,7 +1,26 @@
 ﻿import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static classes = ["hidden"];
+  static values = {
+    isHidden: {type: Boolean, default: true}
+  };
+
+  connect() {
+    if (this.isHiddenValue) {
+      this.element.classList.add(this.hiddenClass);
+    }
+  }
+
+  isHiddenValueChanged(value) {
+    if (value) {
+      this.element.classList.add(this.hiddenClass);
+    } else {
+      this.element.classList.remove(this.hiddenClass);
+    }
+  }
+
   close() {
-    console.log("notification closed")
+    this.isHiddenValue = true;
   }
 }
