8934104d66b1d236f5e3923ebce370ebada3486b
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2022-06-16 17:17:32 +0500

    replace gulp with npm scripts+esbuild

---
+++ src/CanaryOverflow.Service.MVC/scripts/build-css.js
@@ -0,0 +1,7 @@
+﻿const esbuild = require("esbuild");
+
+return esbuild.build({
+  entryPoints: [".\\Views\\application.css"],
+  bundle: true,
+  outfile: ".\\wwwroot\\styles.css"
+});
---
+++ src/CanaryOverflow.Service.MVC/scripts/build-js.js
@@ -0,0 +1,7 @@
+﻿const esbuild = require("esbuild");
+
+return esbuild.build({
+  entryPoints: [".\\Views\\application.js"],
+  bundle: true,
+  outfile: ".\\wwwroot\\bundle.js"
+});
